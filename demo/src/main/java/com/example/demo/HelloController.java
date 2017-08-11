456a309b5c4bb5f3744fbc63210cdc1e228da1d5
ssh382736 <569187731@qq.com>
2017-08-11 10:58:07 +0800

    Update HelloController.java

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -3,8 +3,12 @@ package com.example.demo;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/")
+	@RequestMapping("/hello")
 	public String Hello(){
 		return "Hello,Sun Shihao";
+	}
+	@RequestMapping("/sun")
+	public String sun(){
+		return "SUN test";
 	}
 }
